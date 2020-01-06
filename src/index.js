ba0687ee3596bbc021f0d126c2aa9b004b5ff3bb
Piotr Krasny <krasny.piotr@gmail.com>
2020-01-06 11:39:44 +0100

    remove unused dependencies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-// import 'bootstrap';
 import './index.scss';
 import './images/favicon.ico';
 import 'lazysizes';
-
-// import 'bootstrap/js/dist/util';
-// import 'bootstrap/js/dist/dropdown';
-import 'bootstrap/js/dist/carousel';
-import 'bootstrap/js/dist/collapse';
-
-// fontawesome.library.add(regular);
-// fontawesome.library.add(solid);
-// fontawesome.library.add(brands);
